f6b29662d12e088e3914c3b9b1ed405530d9e64d
Anirudhan Jegannathan <rajegannathan@gmail.com>
2014-07-16 09:55:48 -0700

    warning if index is not initialized

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 angular.module('parser',[])
 .controller('parserController', ['$scope', function($scope){
+  $scope.charAt = -1;
+
   $scope.hai = function(){
+    if($scope.charAt === -1){
+      $scope.parsed = "Incorrect index value.  It cannot be -1";
+      return;
+    }
     var regExp = new RegExp($scope.content.charAt($scope.charAt), "g");
     var escaped = $scope.content.replace(regExp, "\\n");
     console.log('escaped is ', escaped);
