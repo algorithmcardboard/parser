1bf0536ce979a65c8ee7c1a6fe73c2c138559ad6
Anirudhan Jegannathan <rajegannathan@gmail.com>
2014-07-16 09:39:54 -0700

    changes to get the index and use that for replacing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 angular.module('parser',[])
 .controller('parserController', ['$scope', function($scope){
   $scope.hai = function(){
-    var regExp = new RegExp($scope.content.charAt(44), "g");
+    var regExp = new RegExp($scope.content.charAt($scope.charAt), "g");
     var escaped = $scope.content.replace(regExp, "\\n");
     console.log('escaped is ', escaped);
     $scope.parsed = escaped;
