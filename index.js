b20a92b1040a11b429ab842f23b66a08fe5db254
Anirudhan Jegannathan <rajegannathan@gmail.com>
2014-07-16 09:21:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+angular.module('parser',[])
+.controller('parserController', ['$scope', function($scope){
+  $scope.hai = function(){
+    var regExp = new RegExp($scope.content.charAt(44), "g");
+    var escaped = $scope.content.replace(regExp, "\\n");
+    console.log('escaped is ', escaped);
+  };
+}]);
