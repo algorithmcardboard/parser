dda05aefb2ff5082aab7c6b0a344b8fa519c954c
Anirudhan Jegannathan <rajegannathan@gmail.com>
2014-07-16 09:32:00 -0700

    added one more text area for output.  ignoring swp files

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ angular.module('parser',[])
     var regExp = new RegExp($scope.content.charAt(44), "g");
     var escaped = $scope.content.replace(regExp, "\\n");
     console.log('escaped is ', escaped);
-    $scope.content = escaped;
+    $scope.parsed = escaped;
   };
 }]);
