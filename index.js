78e4bb8a0dbd3f3f73127a3333a2a87e08cc08a3
Anirudhan Jegannathan <rajegannathan@gmail.com>
2014-07-16 22:43:51 -0700

    json-ifying the whole content

--- index.js
+++ index.js
@@ -3,15 +3,10 @@ angular.module('parser',[])
   $scope.charAt = -1;
 
   $scope.hai = function(){
-    if($scope.charAt == -1){
-      $scope.parsed = "Incorrect index value.  It cannot be -1";
-      return;
-    }
-    var regExp = new RegExp($scope.content.charAt($scope.charAt), "g");
-    var escaped = $scope.content.replace(regExp, "\\n");
-    console.log('escaped is ', escaped);
     $scope.parsed = JSON.stringify({
-      content:escaped
+      title:$scope.title,
+      author:$scope.author,
+      content:$scope.content
     });
   };
 }]);
